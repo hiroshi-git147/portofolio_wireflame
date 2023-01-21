c460fb52aee2c2b2b751e3a021123509c7e2f6db
hiroshi-git147 <hiroshigit@gmail.com>
2023-01-21 20:06:12 +0900

    fix js file

--- 崩壊学園骨組み/JS/script.js
+++ 崩壊学園骨組み/JS/script.js
@@ -1,10 +1,10 @@
 let activeNav = document.querySelectorAll('li');
 
 for(let i = 0; i <= activeNav.length; i++) {
+	let doubleBorder = document.getElementsByClassName('double-border');
+	let navAwsome = document.getElementsByClassName('nav-awsome');
+	let ancer = document.getElementsByClassName('navigation');
 	activeNav[i].addEventListener('mouseover', () => {
-		var doubleBorder = document.getElementsByClassName('double-border');
-		var navAwsome = document.getElementsByClassName('nav-awsome');
-		var ancer = document.getElementsByClassName('navigation');
 		doubleBorder[i].style.backgroundColor = "#fff";
 		doubleBorder[i].style.border = "solid lightblue 1px";
 		navAwsome[i].style.color = "lightblue";
@@ -12,9 +12,6 @@ for(let i = 0; i <= activeNav.length; i++) {
 	});
 
 	activeNav[i].addEventListener('mouseout', () => {
-		var doubleBorder = document.getElementsByClassName('double-border');
-		var navAwsome = document.getElementsByClassName('nav-awsome');
-		var ancer = document.getElementsByClassName('navigation');
 		doubleBorder[i].style.backgroundColor = "rgb(173, 167, 167)";
 		doubleBorder[i].style.border = "solid rgb(173, 167, 167) 1px";
 		navAwsome[i].style.color = "#fff";
